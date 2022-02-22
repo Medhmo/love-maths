dd0da9457de2f9118310c8c18afe405217de2b93
Medhmo <chrisconde18@gmail.com>
2022-02-22 19:52:55 +0000

    set up structure for the game function

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrimentScore() {
+
+}
+
+function incrimentWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
